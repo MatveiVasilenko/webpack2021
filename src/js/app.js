9d6566ade4b18397b02056fbc0f8695c71d479ea
MatveiVasilenko <mat.vasilenkoo@gmail.com>
2021-07-27 18:44:13 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,34 @@
-import '../scss/app.scss';
+import '../scss/app.scss'
 import 'bootstrap'
+import {DATA} from './data'
+
+const btn = document.querySelector('.search__btn')
+const input = document.querySelector('.search__input')
+const field = document.querySelector('.field__body')
+let result = []
+
+const fetchData = async () => {
+    const response = await fetch('http://127.0.0.1:3008/pages')
+    result = await response.json()
+    
+    return response
+}
+fetchData()
+
+
+btn.onclick = function(){
+    const value = input.value
+    result = result.data.filter((el) => el.title.startsWith(value))
+    result.map((item) => {
+        field.insertAdjacentHTML('beforeend', `
+            <div>${item.title}</div>
+        `)
+    })
+}
+
+//Сделать при нажатии на enter чтобы отрабатывал поиск
+
+//Получаем данные из макуна асинхронно
+//Наполняем DATA 
+//Выводим их на сайт
+//Поиск по дате
---
+++ src/js/data.js
@@ -0,0 +1,16 @@
+// export const DATA = {
+//     data: [
+//         {
+//             id: 1,
+//             title: 'Lorem'
+//         },
+//         {
+//             id: 2,
+//             title: 'Eminem'
+//         },
+//         {
+//             id: 3,
+//             title: 'Light'
+//         },
+//     ]
+// }
